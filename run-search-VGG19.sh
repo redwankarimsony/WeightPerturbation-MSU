67a8710c73e7b90de39878d4951bb34188a1e48c
redwankarimsony <redwankarimsony1455@gmail.com>
2023-11-19 00:31:56 -0500

    config.py

--- run-search-VGG19.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-for VARIABLE in {1..100}
-do
-    python perturb_model-ResNet101.py -modelPath 'Model/LivDet-Iris-2020/VGG19_best.pth' -model 'VGG19' -device "cuda:1" -scales "0.1"
-done
