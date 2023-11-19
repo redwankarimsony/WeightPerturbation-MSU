cbc669841bb6b6b4ab2d7288d4507a55315fb81e
redwankarimsony <redwankarimsony1455@gmail.com>
2023-11-19 11:22:13 -0500

    after wPerturbation script update

--- shell_scripts/run_perturbation.sh
+++ shell_scripts/run_perturbation.sh
@@ -4,23 +4,23 @@
 # Run perturbation search for 100 times for ResNet101
 for VARIABLE in {1..100}
 do
-    python perturb_models.py -model 'ResNet101'
+    python perturb_models_LivDet2020.py -model 'ResNet101'
 done
 
 # Run perturbation search for 100 times for DenseNet161
 for VARIABLE in {1..100}
 do
-    python perturb_models.py -model 'DenseNet161'
+    python perturb_models_LivDet2020.py -model 'DenseNet161'
 done
 
 
 # Run perturbation search for 100 times for VGG19
 for VARIABLE in {1..100}
 do
-    python perturb_models.py -model 'VGG19' -scales "0.1"
+    python perturb_models_LivDet2020.py -model 'VGG19' -scales "0.1"
 done
 
 
 
-
+  
 
